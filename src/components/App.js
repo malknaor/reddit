c916e4781737a56de4e589f88d455fe96a0a9990
malknaor <malknaor@gmail.com>
2019-10-10 12:18:32 +0300

    Temp commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import reddit from './API/reddit';
+
+const renderBestPosts = async () => {
+    const response = await reddit.get('/best')
+    .catch(err => console.log(err));
+
+    console.log(response);
+}
+
+const renderAccountFeatures = async () => {
+    const response = await reddit.get('/api/v1/me.json')
+    .catch(err => console.log(err));
+
+    console.log(response.data.features);
+}
 
 const App = () => {
+    renderBestPosts();
+    renderAccountFeatures();
+
     return (
         <div>
             App
+            <div>
+                Content
+            </div>
         </div>
     );
 };
